072961c7db1d1bad91ee4a08fb8b1b50190ce54a
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-05-17 02:46:14 +0700

    hide screenshot

---
+++ ScreenshotHide/ScreenshotHide/ScreenshotHideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotHideApp.swift
+//  ScreenshotHide
+//
+//  Created by Adrian Suryo Abiyoga on 10/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotHideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
